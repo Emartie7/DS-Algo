2190eb64d3f22ac1939d645076d40391fa83c51f
Emartie7 <edward.m.martinez@gmail.com>
2021-01-01 18:40:59 -0500

    Used "LinkedLists.py" as the basis for for a doubly linked list impelmentation and created "LinkedList_Double.py" for this definition. Added notes to class definitions within "LinkedLists.py"

--- Data Structures/LinkedList_Double.py
+++ Data Structures/LinkedList_Double.py
@@ -1,32 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri Nov 27 19:41:14 2020
+Created on Mon Dec 28
 
 @author: emartie7
 """
 
-#############################################################################
-#### class Node
-####    Defined a "node" object that contains a value and a a singled "link" 
-####    or reference to another object. Intialization routine allows for  
-####    setting a value. Links must be defined externally.
-#############################################################################
+
+#Class for a Node to form a doubly linked list
 class Node():
     def __init__(self,initVal):
         self.value  = None
         self.next   = None
+        self.prev   = None
         if initVal is not None:
             self.value = initVal
-            
-            
-#############################################################################
-#### class LinkedList_Single_Def
-####    Implements a singly linked list utilizinng the "Node" class defined
-####    above. Initialization routine allows a single value to be added to
-####    the list at the instance of object creation.
-#############################################################################
-class LinkedList_Single_Def():
+
+class myLinkedList_Double():
     def __init__(self,InitVal):
         self.list = []
         self.head = Node
@@ -43,6 +33,7 @@ class LinkedList_Single_Def():
     def append(self,NewVal):
         if NewVal is not None:   
             NewNode = Node(NewVal)
+            NewNode.prev = self.tail
             self.list.append(NewNode)              #New item is the tail
             self.tail.next = NewNode
             self.tail = NewNode                   #update tail
@@ -53,6 +44,7 @@ class LinkedList_Single_Def():
             NewNode         = Node(NewVal)
             NewNode.next    = self.head
             self.list.append(NewNode)        #New item must point to previous head
+            self.head.prev = NewNode
             self.head = NewNode
             self.writeIdx += 1
 
@@ -77,6 +69,17 @@ class LinkedList_Single_Def():
                 break
             else:
                 ItemCt += 1
+    
+    def PrintListReverse(self):
+        CurrItem    = self.tail
+        ItemCt      = 0
+        while CurrItem is not None:
+            print(CurrItem.value)
+            CurrItem = CurrItem.prev
+            if(ItemCt > self.writeIdx):
+                break
+            else:
+                ItemCt += 1
               
     def Insert(self,NewVal,Idx):
         if NewVal is not None:
@@ -91,41 +94,47 @@ class LinkedList_Single_Def():
                 FollowRef = LeaderRef.next              #Node currently at position N
                 
                 NewNode.next = FollowRef
+                NewNode.prev = LeaderRef
+                
                 self.list.append(NewNode)
                 LeaderRef.next = NewNode
+                FollowRef.prev = NewNode
                 self.writeIdx +=1
                 
-    def remove(self,Idx):
+    def Remove(self,Idx):
         if(Idx == 0):
-            self.head = self.head.next
+            self.head       = self.head.next
+            self.head.prev  = None
             self.writeIdx -= 1
-        elif Idx < self.writeIdx:
-            LeaderRef = self.traverseToIdx(Idx-1)   # Node at N-1
-            RemoveRef = LeaderRef.next              # Node to be deleted at N
             
-            LeaderRef.next = RemoveRef.next  #Tell leader to point to node at N+1
+        elif Idx == self.writeIdx:
+            TailRef = self.tail.prev
+            TailRef.next = None
+            self.tail = TailRef
             self.writeIdx -= 1
-            if RemoveRef.next is None:
-                self.tail = LeaderRef
+            
+        elif Idx < self.writeIdx:
+            RemoveRef = self.traverseToIdx(Idx)
+            FollowRef = RemoveRef.next              # Node to be deleted at N
+            LeaderRef = RemoveRef.prev
+            
+            LeaderRef.next = FollowRef
+            FollowRef.prev = LeaderRef
+                  
+            self.writeIdx -= 1
+            
         else:
             return print("\nRequested index is out of range\n")
+                       
             
-            
-            
-                
-            
-            
-            
-testList = LinkedList_Single_Def(1)
+testList = myLinkedList_Double(1)
 testList.append(2)
 testList.append(3)
 testList.prepend(4)
 testList.append(5)
 testList.prepend(6)
-testList.PrintList()
 testList.Insert(7, 3)
-print("\n")
+testList.Remove(7)
 testList.PrintList()
-testList.remove(15)
-print("\n")
-testList.PrintList()          
+print("\nList in reverse is: \n")
+testList.PrintListReverse()     
