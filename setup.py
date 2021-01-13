b2a5e7cb302e88d998daf8b3da0c67d5f2057afa
Emartie7 <edward.m.martinez@gmail.com>
2021-01-12 21:38:19 -0500

    Added __init__.py and setup.py to setup python module for data structures definitions. Added requirements.txt to provide visibility of dependencies and installation requirements for virtual environments.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='DSdefs',
+        version = '1.0',
+        description='Linked Lists Lib',
+        author='Edward Martinez',
+        email='edward.m.martinez@gmail.com',
+        package_dir={'': 'Data Structures'},
+        py_modules = ['Arrays','HashTables','LinkedLists','LinkedList_Double'],
+)
