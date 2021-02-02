ab7d99b4a96fbf778b005372052945f001918d3e
Emartie7 <edward.m.martinez@gmail.com>
2021-02-01 21:46:58 -0500

    Added class definitions for an array (python list) - based stack and a linked list-based queue. Implemented preliminary tests in Stacks_Queues_Exercise.py

--- Data Structures/Stacks_Queues.py
+++ Data Structures/Stacks_Queues.py
@@ -49,4 +49,94 @@ class Stack():
             self.top = self.NodeList.tail
             self.bottom = self.NodeList.head
             return None
+
+class Stack2():
+    def __init__(self,InitVal=None):
+        self.StackList = []
+        for x in range(0,MAX_STACK_LEN-1):
+            self.StackList.append(x)
         
+        self.top = None
+        self.bottom = None
+        self.length = 0
+        
+        if InitVal is not None:
+            self.StackList[self.length] = InitVal
+            self.top = InitVal
+            self.bottom = InitVal
+    
+    def peek(self):
+        return self.top
+    
+    def push(self,NewVal):
+        
+        if NewVal is not None:
+            self.StackList[self.length] = NewVal
+            if self.length == 0:          
+                self.top = NewVal
+                self.bottom = NewVal
+            else:
+                self.top = NewVal
+            
+            self.length += 1
+    
+    def pop(self):
+        popped = None
+        
+        if self.length > 1:
+            popped = self.top
+            self.length -= 1
+            self.top = self.StackList[self.length-1]
+            
+        elif self.length == 1:
+            popped = self.top
+            self.length = 0
+            self.top = None
+            self.bottom = None
+        return popped
+            
+
+class Queue():
+    def __init__(self,InitVal=None):
+        self.length = 0
+        self.List = LinkedLists.LinkedList_Single_Def(InitVal)
+        self.top = self.List.head
+        self.bottom = self.List.tail
+        if InitVal is not None:
+            self.length += 1
+    
+    def Queue(self,NewVal):
+        self.List.append(NewVal)
+        self.top = self.List.head
+        self.bottom = self.List.tail
+        self.length += 1
+        return self.bottom
+    
+    def Dequeue(self):
+        if self.length > 0:
+            HoldingVal = self.List.head
+            self.List.remove(0)
+            self.top = self.List.head
+            self.bottom = self.List.tail
+            self.length -= 1
+            return HoldingVal.value
+        else:
+            return None
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
--- Exercises/Stacks_Queues_Exercise.py
+++ Exercises/Stacks_Queues_Exercise.py
@@ -6,22 +6,50 @@ Created on Mon Jan 25 18:14:37 2021
 @author: emartie7
 """
 
-from Stacks_Queues import Stack
+from Stacks_Queues import Stack, Stack2, Queue
 
-myStack = Stack(5)
-print("\nTop value is: {}, length is: {}".format(myStack.top.value,myStack.length))
-myStack.push(8)
-myStack.push(15)
-myStack.push(103)
-myStack.push(78)
-print("\nTop value is: {}, length is: {}".format(myStack.peek(),myStack.length))
-# # print("\nPopping value: {}".format(myStack.pop()))
-# # myStack.pop()
+# myStack = Stack(5)
 # print("\nTop value is: {}, length is: {}".format(myStack.top.value,myStack.length))
+# myStack.push(8)
+# myStack.push(15)
+# myStack.push(103)
+# myStack.push(78)
+# print("\nTop value is: {}, length is: {}".format(myStack.peek(),myStack.length))
+# # # print("\nPopping value: {}".format(myStack.pop()))
+# # # myStack.pop()
+# # print("\nTop value is: {}, length is: {}".format(myStack.top.value,myStack.length))
 
-popped = 0
-while popped is not None:
-    popped = myStack.pop()
-    print("\nPopped Value: {}; New length is: {}".format(popped,myStack.length))
+# popped = 0
+# while popped is not None:
+#     popped = myStack.pop()
+#     print("\nPopped Value: {}; New length is: {}".format(popped,myStack.length))
 
-print("\nTop value is: {}, bottom value is: {}".format(myStack.top.value,myStack.bottom))
+# print("\nTop value is: {}, bottom value is: {}".format(myStack.top.value,myStack.bottom))
+
+# myStack = Stack2(8)
+# myStack.push(13)
+# myStack.push(39)
+# myStack.push(88)
+# # print(myStack.peek())
+# print(myStack.pop())
+# print(myStack.pop())
+# print(myStack.pop())
+# print(myStack.pop())
+# print(myStack.pop())
+# print(myStack.pop())
+# print(myStack.push(67))
+# print(myStack.peek())
+
+myQueue = Queue("Susan")
+myQueue.Queue("Kristaps")
+myQueue.Queue("Luka")
+myQueue.Queue("Dbook")
+print(myQueue.Dequeue())
+myQueue.Queue("CP3")
+print(myQueue.Dequeue())
+print(myQueue.Dequeue())
+print(myQueue.Dequeue())
+print(myQueue.Dequeue())
+print(myQueue.Dequeue())
+print(myQueue.Dequeue())
+print(myQueue.Dequeue())
