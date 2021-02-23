11a6c853aa1bd38c789977425b61791a2aa31cbc
Emartie7 <edward.m.martinez@gmail.com>
2021-02-22 19:44:53 -0500

    Added methods for looking up and removing values in Tree data structure. Current tree logic will move "left" nodes up first when removing a node with two children.

--- Data Structures/Trees.py
+++ Data Structures/Trees.py
@@ -8,9 +8,10 @@ Created on Tue Feb 16 20:22:33 2021
 
 class NodeT:
     def __init__(self,initVal=None):
-        self.left = None
-        self.right = None
-        self.value = initVal
+        self.parent = None
+        self.left   = None
+        self.right  = None
+        self.value  = initVal
 
 class BinarySearchTree():
     def __init__(self,initVal=None):
@@ -22,12 +23,7 @@ class BinarySearchTree():
             self.NumLeafs   = 0
             self.levels     = 1
         
-    def insert(self,NewVal):
-        # if NewVal > self.root.value:
-        #     self.root.right = NodeT(NewVal)
-        # else:
-        #     self.root.left = NodeT(NewVal)
-            
+    def insert(self,NewVal):            
         currNode = self.root
         test = 0
         while currNode is not None:
@@ -35,45 +31,126 @@ class BinarySearchTree():
                 compareNode = currNode.right        #Get reference to right leaf
                 
                 if compareNode is None:
-                    currNode.right = NodeT(NewVal)  #Update right leaf reference
+                    NewNode = NodeT(NewVal)
+                    NewNode.parent = currNode
+                    currNode.right = NewNode  #Update right leaf reference
                     self.NumLeafs += 1
                     self.levels += 1
                     currNode = None
                 else:
-                    # if NewVal < compareNode.value:
-                    #     NewNode = NodeT(NewVal)
-                    #     NewNode.right = compareNode
-                    #     currNode.right = NewNode
-                    # else:
                     currNode = compareNode                   
             else:
                 compareNode = currNode.left         #Get reference to left leaf
                 
                 if compareNode is None:
-                    currNode.left = NodeT(NewVal)  #Update left leaf reference
+                    NewNode = NodeT(NewVal)
+                    NewNode.parent = currNode
+                    currNode.left = NewNode
                     self.NumLeafs += 1
                     self.levels += 1
                     currNode = None
                 else:                        
                     currNode = compareNode
-            
-            # if compareNode is None:
-            #     compareNode = NodeT(NewVal)
-            #     self.NumLeafs += 1
-            #     currNode = None
-            # else:
-            #     currNode = compareNode
+
             test += 1
             if test > self.NumLeafs:
                 currNode = None
                 print("Error, entered an infinite loop. Breaking out now. . .")
-
-            
+                
+    def lookup(self,LookupVal):
+        CurrNode = self.root
+        NextNode = self.root
+        test = 0
+        while(CurrNode is not None):
+            print("Current Node value is: {}".format(CurrNode.value))
+            if(CurrNode.value == LookupVal):
+                print("Found node with value: {}".format(LookupVal))
+                return CurrNode
+                # NextNode = None
+            else:
+                if(LookupVal>CurrNode.value):
+                    #go right
+                    NextNode = CurrNode.right
+                else:
+                    NextNode = CurrNode.left
+            CurrNode = NextNode
+                
+            test += 1
+            if test > self.NumLeafs:
+                CurrNode = None
+                print("Error, entered an infinite loop or value not found. Breaking out now. . .")
+        if NextNode is None:
+            print("Warning: Value {} not found.".format(LookupVal))
+        return CurrNode
     
-myTree = BinarySearchTree(10)
+    def remove(self,RemoveValue):
+        RemoveNode = self.lookup(RemoveValue)
+        if RemoveNode is not None:
+            #remove that shit
+            ParentRef = RemoveNode.parent
+            if RemoveNode.left is not None:                 #Remove node to be replaced by left branch 
+                if RemoveNode == ParentRef.left:
+                    ParentRef.left = RemoveNode.left        #Update parent left branch pointers
+                    ParentRef.left.parent = ParentRef
+                    UpdateNode = ParentRef.left
+                else:
+                    ParentRef.right = RemoveNode.right
+                    ParentRef.right.parent = ParentRef
+                    UpdateNode = ParentRef.right
+                if RemoveNode.right is not None:            #Update Right node parent if it exists (left and right node exist)
+                    UpdateNode.right = RemoveNode.right
+                    RemoveNode.right.parent = UpdateNode
+            elif(RemoveNode.right is not None):             #Remove Node to be replaced by right branch
+                if RemoveNode == ParentRef.left:
+                    #Update parent left branch pointers
+                    ParentRef.left = RemoveNode.right
+                    RemoveNode.parent = ParentRef
+                else:
+                    #Update parent right branch pointers
+                    ParentRef.right = RemoveNode.right
+                    RemoveNode.parent = ParentRef
+            else:
+                #No children on remove node
+                if RemoveNode == ParentRef.left:
+                    ParentRef.left = None
+                else:
+                    ParentRef.right = None
+                
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                # if RemoveNode.right is None:    #No right branch on remove node
+                #     if RemoveNode == ParentRef.left:
+                #         ParentRef.left = RemoveNode.left    #Update parent left branch pointers
+                #         ParentRef.left.parent = ParentRef
+                #     else:
+                #         ParentRef.right = RemoveNode.right
+                #         ParentRef.right.parent = ParentRef
+                # else:
+                #     if RemoveNode == ParentRef.left:
+                #         ParentRef.left = RemoveNode.left    #Update parent left branch pointers
+                #         ParentRef.left.parent = ParentRef
+                #     else:
+                #         ParentRef.right = RemoveNode.right
+                #         ParentRef.right.parent = ParentRef
+        else:
+            return None
+
+
+
+myTree = BinarySearchTree(9)
+myTree.insert(4)
+myTree.insert(20)
+myTree.insert(1)
+myTree.insert(6)
 myTree.insert(15)
-myTree.insert(5)
-myTree.insert(9)
-myTree.insert(7)
-myTree.insert(11)
-print(myTree.root.right.value)
+myTree.insert(170)
+myTree.lookup(15)
+myTree.lookup(6)
+myTree.remove(4)
+RetrievedNode = myTree.lookup(1)
+print("Found node with value: {}, left child: {}, right child: {}, and parent {}".format(RetrievedNode.value,RetrievedNode.left,RetrievedNode.right,RetrievedNode.parent.value))
