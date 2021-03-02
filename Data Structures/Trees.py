63725d7f8b566350fbfa324f2b26e8c28ad55e4a
Emartie7 <edward.m.martinez@gmail.com>
2021-03-01 21:53:45 -0500

    Updated logic in BST method "remove" to update logic when a left-most node is being "moved" up in the tree, but also has a right child. Right child will be linked to the former parent. Updated BST "insert" method to handle case where the Tree is created, but left empty (root is None).

--- Data Structures/Trees.py
+++ Data Structures/Trees.py
@@ -15,48 +15,62 @@ class NodeT:
 
 class BinarySearchTree():
     def __init__(self,initVal=None):
-        self.root = NodeT(initVal)
+        self.root = None
         if initVal is not None:
+            self.root = NodeT(initVal)
             self.NumLeafs   = 1
             self.levels     = 1
         else:
             self.NumLeafs   = 0
             self.levels     = 1
-        
-    def insert(self,NewVal):            
-        currNode = self.root
-        test = 0
-        while currNode is not None:
-            if NewVal > currNode.value:
-                compareNode = currNode.right        #Get reference to right leaf
-                
-                if compareNode is None:
-                    NewNode = NodeT(NewVal)
-                    NewNode.parent = currNode
-                    currNode.right = NewNode  #Update right leaf reference
-                    self.NumLeafs += 1
-                    self.levels += 1
-                    currNode = None
+            
+    #############################################################################
+    #### def insert(self,NewVal)
+    ####    Creates a new node containing the value "NewVal" then inserts it into
+    ####    the appropriate location in the BST.
+    #############################################################################
+    def insert(self,NewVal):
+        if self.root is not None:
+            currNode = self.root
+            test = 0
+            while currNode is not None:
+                if NewVal > currNode.value:
+                    compareNode = currNode.right        #Get reference to right leaf
+                    
+                    if compareNode is None:
+                        NewNode = NodeT(NewVal)
+                        NewNode.parent = currNode
+                        currNode.right = NewNode  #Update right leaf reference
+                        self.NumLeafs += 1
+                        self.levels += 1
+                        currNode = None
+                    else:
+                        currNode = compareNode                   
                 else:
-                    currNode = compareNode                   
-            else:
-                compareNode = currNode.left         #Get reference to left leaf
-                
-                if compareNode is None:
-                    NewNode = NodeT(NewVal)
-                    NewNode.parent = currNode
-                    currNode.left = NewNode
-                    self.NumLeafs += 1
-                    self.levels += 1
+                    compareNode = currNode.left         #Get reference to left leaf
+                    
+                    if compareNode is None:
+                        NewNode = NodeT(NewVal)
+                        NewNode.parent = currNode
+                        currNode.left = NewNode
+                        self.NumLeafs += 1
+                        self.levels += 1
+                        currNode = None
+                    else:                        
+                        currNode = compareNode
+    
+                test += 1
+                if test > self.NumLeafs:
                     currNode = None
-                else:                        
-                    currNode = compareNode
-
-            test += 1
-            if test > self.NumLeafs:
-                currNode = None
-                print("Error, entered an infinite loop. Breaking out now. . .")
+                    print("Error, entered an infinite loop. Breaking out now. . .")
+        else:
+            self.root = NodeT(NewVal)
                 
+    #############################################################################
+    #### def lookup(self,LookupVal)
+    ###     Searches the BST for the value specified by "LookupVal" and returns the
+    ###     node containing this value if found, "None" if not.
+    #############################################################################
     def lookup(self,LookupVal):
         CurrNode = self.root
         NextNode = self.root
@@ -83,6 +97,11 @@ class BinarySearchTree():
             print("Warning: Value {} not found.".format(LookupVal))
         return CurrNode
     
+    #############################################################################
+    #### def findMinimum(self,subRoot)
+    ####    Given a starting node in a tree, this routine will return the leftmost
+    ####    node ("minimum") in the tree/subtree.
+    #############################################################################
     def findMinimum(self,subRoot):
         CurrNode = subRoot
         NextNode = subRoot.left
@@ -92,27 +111,42 @@ class BinarySearchTree():
         print("Minimum tree value is: {}".format(CurrNode.value))
         return CurrNode
     
+    #############################################################################
+    #### def remove(self,RemoveValue)
+    ####    Looks up the value to be removed, "RemoveValue", then proceeds to remove
+    ####    all links to/from the node containing the specified value. If the removed
+    ####    has two children, find the left-most value in the subtree and this node
+    ###     will take the place of the removed node.
+    #############################################################################
     def remove(self,RemoveValue):
         RemoveNode = self.lookup(RemoveValue)
         if RemoveNode is not None:
-            #Remove if node was found
+            
+            ################ Case 1: No Children ################
             ParentRef = RemoveNode.parent
-            if ((RemoveNode.left is None) and (RemoveNode.right is None)):              #No children
+            if ((RemoveNode.left is None) and (RemoveNode.right is None)):
                 if RemoveNode == ParentRef.left:
                     ParentRef.left = None
                 else:
                     ParentRef.right = None
-            elif((RemoveNode.left is not None) and (RemoveNode.right is not None)):     #Two Children
+            ################ Case 2: Two children ################
+            elif((RemoveNode.left is not None) and (RemoveNode.right is not None)):
                 MinNode = self.findMinimum(RemoveNode)
                 if RemoveNode == ParentRef.left:
                     ParentRef.left = MinNode        #Minimum value takes the place of the removed value
                 else:
                     ParentRef.right = MinNode
+                    
+                MinParent = MinNode.parent      #Get a reference to the parent of the minimum value
+                if MinNode.right is not None:
+                    MinParent.left = MinNode.right           
+                else:
+                    MinParent.left = None           #Former parent of minimum value loses its left  child
                 MinNode.left = RemoveNode.left
                 MinNode.right = RemoveNode.right
-                MinParent = MinNode.parent      #Get a reference to the parent of the minimum value
+                
                 MinNode.parent = ParentRef      #Update the parent of the minimum node (now in the place of the removed node)
-                MinParent.left = None           #Former parent of minimum value loses its left  child 
+            ################ Case 3: One child ################
             else:
                 if RemoveNode.left is not None:             #Removed node has left child
                     if RemoveNode == ParentRef.left:
@@ -124,6 +158,8 @@ class BinarySearchTree():
                         ParentRef.left = RemoveNode.right 
                     else:
                         ParentRef.right = RemoveNode.right
+                        
+                        
             return RemoveNode              
         else:
             return None
@@ -139,6 +175,7 @@ myTree.insert(15)
 myTree.insert(170)
 myTree.insert(13)
 myTree.insert(19)
+myTree.insert(14)
 myTree.remove(20)
 # myTree.findMinimum(myTree.root)
 # print("Found node with value: {}, left child: {}, right child: {}, and parent {}".format(RetrievedNode.value,RetrievedNode.left,RetrievedNode.right,RetrievedNode.parent.value))
